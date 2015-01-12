6b50831bf21b85134f3e74521a015b4c9800e82a
Jevgeni V <jevgeniv@gmail.com>
2015-01-12 19:30:42 +0200

    minor refactor; remove unnecessary click.

--- src/com/nortal/course/selenium/TrivialSearchTest.java
+++ src/com/nortal/course/selenium/TrivialSearchTest.java
@@ -24,22 +24,15 @@ public class TrivialSearchTest {
         initWebDriver();
 
 		/* protocol specification is important - http:// */
-        driver.get("http://www.neti.ee");
+        driver.navigate().to("http://www.neti.ee"); // ~ driver.get("http://www.neti.ee");
 
-        {
-            WebElement queryInput =
-                    driver.findElement(By.id("search-bar")).findElement(By.cssSelector("input.input[name='query']"));
-            queryInput.click();
-            queryInput.sendKeys("Nortal");
-        }
-        driver.findElement(By.cssSelector("form[action='/cgi-bin/otsing'] input.button[type='submit']")).click();
-
+        searchFor("Nortal");
 
         // verify something..
-        List<WebElement> nortalLinks = driver.findElements(new ByChained(By.className("result-item"), By.partialLinkText("Nortal")));
+        List<WebElement> nortalLinks =
+                driver.findElements(new ByChained(By.className("result-item"), By.partialLinkText("Nortal")));
         Assert.assertTrue("there should be more than 1 link with 'Nortal' word", nortalLinks.size() > 1);
         WebElement firstNortalLink = nortalLinks.get(0);
-
 
         Assert.assertTrue("First link in search result should lead to 'www.nortal.ee'",
                 firstNortalLink.getAttribute("href").contains("www.nortal.ee"));
@@ -49,5 +42,13 @@ public class TrivialSearchTest {
 
         // finalize stuff, closes driver/browser
         driver.close();
+    }
+
+    private void searchFor(String keyword) {
+        WebElement queryInput =
+                driver.findElement(By.id("search-bar")).findElement(By.cssSelector("input.input[name='query']"));
+        queryInput.sendKeys(keyword);
+
+        driver.findElement(By.cssSelector("form[action='/cgi-bin/otsing'] input.button[type='submit']")).click();
     }
 }
