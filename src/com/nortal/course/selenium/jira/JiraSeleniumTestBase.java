6064d61e338f099b5d439c8522eb49517c736333
Jevgeni V <jevgeniv@gmail.com>
2015-02-02 20:56:31 +0200

    selenium config support, personal & common selenium properties

--- src/com/nortal/course/selenium/jira/JiraSeleniumTestBase.java
+++ src/com/nortal/course/selenium/jira/JiraSeleniumTestBase.java
@@ -1,6 +1,10 @@
 package com.nortal.course.selenium.jira;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by jevgeni on 1/30/15.
@@ -8,10 +12,22 @@ import org.junit.BeforeClass;
 public class JiraSeleniumTestBase {
     private static SeleniumTestConfig config;
 
+    private WebDriver driver;
+
     @BeforeClass
     public static void init() {
         config = new SeleniumTestConfig();
         config.init();
+    }
+
+    @Before
+    public void setUpSelenium() {
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void tearDownSelenium() {
+        driver.quit();
     }
 
     public String getConfigParameter(String paramKey) {
@@ -28,5 +44,9 @@ public class JiraSeleniumTestBase {
 
     public String getJiraPassword() {
         return getConfigParameter("selenium.jira.password");
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
     }
 }
--- src/com/nortal/course/selenium/jira/SeleniumTestConfig.java
+++ src/com/nortal/course/selenium/jira/SeleniumTestConfig.java
@@ -11,12 +11,22 @@ public class SeleniumTestConfig {
     private Properties properties;
 
     public void init() {
-        properties = new Properties();
+        properties = new Properties(makePropertiesFromFile("selenium-common-rc.properties"));
+        loadPropertiesFromFile(properties, "selenium-rc.properties");
+    }
+
+    private void loadPropertiesFromFile(Properties p, String filename) {
         try {
-            properties.load(getClass().getClassLoader().getResourceAsStream("selenium-rc.properties"));
+            p.load(getClass().getClassLoader().getResourceAsStream(filename));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error reading properties from file " + filename, e);
         }
+    }
+
+    private Properties makePropertiesFromFile(String filename) {
+        Properties p = new Properties();
+        loadPropertiesFromFile(p, filename);
+        return p;
     }
 
     public String getProperty(String key) {
